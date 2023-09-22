c5411447fb25ad4f83ecfe04e602571bff79c757
amilaveer <95239716+amilaveer@users.noreply.github.com>
2023-09-23 02:07:35 +0300

    a

--- index.js
+++ index.js
@@ -25,16 +25,12 @@ http.createServer(function (req, res) {
     })
 }).listen(process.env.PORT || 3000);
 
-getUrls = (str, lower = false) => {
+getUrls = (fileText, lower = false) => {
     const regexp = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()'@:%_\+.~#?!&//=]*)/gi;
     const bracketsRegexp = /[()]/g;
   
-    if (typeof str !== "string") {
-      throw new TypeError(`The str argument should be a string, got ${typeof str}`);
-    }
-  
-    if (str) {
-      let urls = str.match(regexp);
+    if (fileText) {
+      let urls = fileText.match(regexp);
       if (urls) {
         return lower ? urls.map((item) => item.toLowerCase().replace(bracketsRegexp, "")) : urls.map((item) => item.replace(bracketsRegexp, ""));
       } else {
