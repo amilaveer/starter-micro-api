19faf3e711404883d8eccaa82d2085137a554652
amilaveer <95239716+amilaveer@users.noreply.github.com>
2023-09-22 22:34:49 +0300

    asa

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var http = require('http');
-const extractUrls = require("./node_modules/extract-urls/index.js");
+
 http.createServer(function (req, res) {
 
     let data = [];
@@ -14,7 +14,7 @@ http.createServer(function (req, res) {
         dataMap.forEach((element) => {
             ids.push(element.Id);
             let text = `You can read https://github.com/huckbit/extract-urls or https://www.npmjs.com/package/extract-urls for more info`;
-            urls = extractUrls(text);
+            urls = getUrls(text);
         });
   
 
@@ -23,3 +23,23 @@ http.createServer(function (req, res) {
       res.end()
     })
 }).listen(process.env.PORT || 3000);
+
+getUrls = (str, lower = false) => {
+    const regexp = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()'@:%_\+.~#?!&//=]*)/gi;
+    const bracketsRegexp = /[()]/g;
+  
+    if (typeof str !== "string") {
+      throw new TypeError(`The str argument should be a string, got ${typeof str}`);
+    }
+  
+    if (str) {
+      let urls = str.match(regexp);
+      if (urls) {
+        return lower ? urls.map((item) => item.toLowerCase().replace(bracketsRegexp, "")) : urls.map((item) => item.replace(bracketsRegexp, ""));
+      } else {
+        undefined;
+      }
+    } else {
+      undefined;
+    }
+  };
